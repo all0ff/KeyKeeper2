d8bf4c14ad3b04069063be572b893d0d49774c65
all0ff <ustass.al@gmail.com>
2026-09-06 21:11:51 +0300

    исправление ошибок с ПИН

--- components/ui/src/screens/quick_screen.cpp
+++ components/ui/src/screens/quick_screen.cpp
@@ -78,7 +78,7 @@ bool QuickScreen::on_input(InputAction action)
             }
 
             manager().push(std::make_unique<MainMenu>());
-            return false; // QuickScreen is always stack-bottom; pop() is a no-op anyway
+            return true; // QuickScreen is always stack-bottom; pop() is a no-op anyway
 
         case InputAction::BackLong: {
             const security::permission::Result perm =
